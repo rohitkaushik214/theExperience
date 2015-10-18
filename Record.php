25cc2fcca84b2a5b6eccdd1e81f4319503921735
rohitkaushik214 <rohitkaushik214@gmail.com>
2015-10-18 22:10:21 +0530

    Adding code for Record.php

--- Record.php
+++ Record.php
@@ -1,52 +1,94 @@
 <?php
 
 //Parent class of all the records and models.
- public class Record
+require_once 'IRecord.php';
+class Record implements IRecord
 {
 	private $oRecord;
 	private $sRecordType;
+	private $oDatabaseConnection;
 
-	public function createRecord($oRecord, $sRecordType)
+	public function __construct(IRecord $oRecord)
 	{
+		$this->oRecord     = $oRecord;
+		$this->sRecordType = get_class($oRecord);
+		if($this->oDatabaseConnection == NULL)
+		{
+			$this->oDatabaseConnection = new DatabaseOps();
+		}
+	}
+
+	public function handleFailedSQLOperation($sQuery, $oResult)
+	{
+		if(array_key_exists('message', $oResult->aFetchResults)) {
+			$sExceptionMessage = $oResult->aFetchResults['message'];
+		}
+		if(array_key_exists('stackTrace', $oResult->aFetchResults)) {
+			$sExceptionStackTrace = $oResult->aFetchResults['stackTrace'];
+		}
+		/*
+         * @Todo: Log the exception and the query.
+         */
+	}
+
+	public function createRecord()
+	{
+		$bSuccess = TRUE;
+		$sQuery   = $this->oDatabaseConnection->prepareInsertQuery($this->sRecordType, $this->oRecord);
+		$oResult  = $this->oDatabaseConnection->fireQuery($sQuery, $this->oRecord, FALSE);
+
+		if($oResult->bSuccess == FALSE) {
+			$bSuccess = FALSE;
+			$this->handleFailedSQLOperation($sQuery,$oResult);
+		}
+		return $bSuccess;
+	}
+
+	public function getRecords($aSelectColumns, $bUseFilterLogic, $sFilterLogic, $aFilters, $bUseTop, $iTop, $sGroupBy,
+							   $sOrderByColumn, $sOrderBySequence )
+	{
+		$bSuccess = TRUE;
+		$sQuery   = $this->oDatabaseConnection->prepareSelectQuery($this->sRecordType, $aSelectColumns,
+			$bUseFilterLogic, $sFilterLogic, $aFilters, $bUseTop, $iTop, $sGroupBy, $sOrderByColumn, $sOrderBySequence);
+		$oResult  = $this->oDatabaseConnection->fireQuery($sQuery, $this->oRecord, TRUE);
+
+		if($oResult->bSuccess == FALSE || !is_array($oResult->aFetchResults) || empty($oResult->aFetchResults)) {
+			$bSuccess = FALSE;
+			$this->handleFailedSQLOperation($sQuery,$oResult);
+			return $bSuccess;
+		}
+
+		return $oResult->aFetchResults;
+	}
+
+	public function updateRecord($aUpdateRecords, $aFilters)
+	{
+		/*
+		 * @Todo: Re-think about it.
+		 */
+		$bSuccess = TRUE;
+		$sQuery   = $this->oDatabaseConnection->prepareUpdateQuery($this->sRecordType, $aUpdateRecords, $aFilters);
+		$oResult  = $this->oDatabaseConnection->fireQuery($sQuery, $aFilters, FALSE);
+		if($oResult->bSuccess == FALSE) {
+			$bSuccess = FALSE;
+			$this->handleFailedSQLOperation($sQuery,$oResult);
+		}
+
+		return $bSuccess;
 
 	}
 
-
-	public function determineRecordType($oRecord)
+	public function deleteRecord($aRecordIds, $sRecordIDParameter)
 	{
-		//return and validate record type
-		
+		$bSuccess = TRUE;
+		$sQuery   = $this->oDatabaseConnection->prepareDeleteQuery($this->sRecordType, $aRecordIds, $sRecordIDParameter);
+		$oResult  = $this->oDatabaseConnection->fireQuery($sQuery, $aRecordIds, FALSE);
+		if($oResult->bSuccess == FALSE) {
+			$bSuccess = FALSE;
+			$this->handleFailedSQLOperation($sQuery,$oResult);
+		}
 
-	}
-
-	public function updateRecord()
-	{
-
-	}
-
-	public function deleteRecord()
-	{
-
-	}
-
-	public function createRecords()
-	{
-
-	}
-
-	public function updateRecords()
-	{
-
-	}
-
-	public function deleteRecords()
-	{
-
-	}
-
-	public function storeRecord()
-	{
-
+		return $bSuccess;
 	}
 
 }
